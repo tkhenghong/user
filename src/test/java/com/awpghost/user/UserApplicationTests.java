d06ce46b40e30a39b91b874d72a28a99fb20d155
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-03-09 00:47:38 +0800

    first commit

---
+++ src/test/java/com/awpghost/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awpghost.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
