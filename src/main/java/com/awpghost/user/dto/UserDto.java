58420b33d653b46fb05c380f1015aae6d13a29cf
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-03-09 01:00:40 +0800

    Minor edit

---
+++ src/main/java/com/awpghost/user/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.awpghost.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String location;
+    private String mobileNo;
+}
